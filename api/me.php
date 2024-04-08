74b82dd81445565b70bb7bb32511f48b341f610a
jazeabby <abby@escape.tech>
2024-04-08 14:56:45 +0200

    adding api speicification for dummy api

---
+++ api/me.php
@@ -0,0 +1,39 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+
+// all of our endpoints start with /person
+// everything else results in a 404 Not Found
+if ($uri[2] !== 'me') {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
+
+// the user id is, of course, optional and must be a number:
+$userId = null;
+if (isset($uri[2])) {
+    $userId = (int) $uri[2];
+}
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+// echo "I am healthy!";
+
+echo json_encode([
+    "id": 10,
+    "username": "theUser",
+    "firstName": "John",
+    "lastName": "James",
+    "email": "john@email.com",
+    "password": "12345",
+    "phone": "12345",
+    "userStatus": 1
+]);
+exit();
